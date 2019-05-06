d73c19d89f7a6cce8d541506f9a5820cf97da4fd
背对疾风吧 <my364007886@gmail.com>
2019-05-06 12:16:19 +0900

    delete tem files

--- elpa/popup-20160709.1429/popup-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("popup.el") (23391 7663 59995 542000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
